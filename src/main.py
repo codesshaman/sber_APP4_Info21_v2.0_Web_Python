940f47678403226f2cb1264e351e356c28cbf732
codesshaman <nicksemail@yandex.ru>
2023-03-01 20:50:51 +0300

    remove old

--- src/main.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, url_for
-from controllers.routes import app_route
-
-app = Flask(__name__)
-app.register_blueprint(app_route)
-
-if __name__ == '__main__':
-    app.run(debug=True)
