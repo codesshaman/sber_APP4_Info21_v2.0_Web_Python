6555a06119a4c278cd8bb39a229163d098ec5a56
codesshaman <nicksemail@yandex.ru>
2023-03-02 17:35:32 +0300

    connection to postgres

--- src/project/config.py
+++ src/project/config.py
@@ -1,5 +1,5 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://postgres/[postgres]")
+SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgres://postgres@postgres:5432/postgres")
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres/[postgres]'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
--- src/project/main.py
+++ src/project/main.py
@@ -4,7 +4,8 @@ from controllers.db_connection import connect
 
 
 app = Flask(__name__)
-app.config.from_pyfile("config.py")
+# app.config.from_pyfile("config.py")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@postgres:5432/postgres'
 # connect.init_app(app)
 app.register_blueprint(app_route)
 
