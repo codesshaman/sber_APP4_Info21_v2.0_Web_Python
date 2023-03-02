12c8baf11b821c55d1bf1ba33d3f8c8b8c3a98ef
codesshaman <nicksemail@yandex.ru>
2023-03-02 16:49:13 +0300

    change config

--- src/project/config.py
+++ src/project/config.py
@@ -1,4 +1,5 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgres://http://postgres:5432")
+SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://postgres/[postgres]")
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres/[postgres]'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
