a3e753b13b1411e37aa036c30d78f40d31d866a7
codesshaman <nicksemail@yandex.ru>
2023-04-03 17:47:58 +0300

    add edit

--- src/project/model/templates/datapage.py
+++ src/project/model/templates/datapage.py
@@ -2,11 +2,21 @@ from model.database.sql_query import *
 
 def table_titles(table):
     arr = fetchall_query(f"SELECT column_name FROM information_schema.columns WHERE table_name = '{table}' ORDER BY ordinal_position;")
-    return arr
+    list_arr = list(arr)
+    list_arr.append("('action',)")
+    print(list_arr)
+    return list_arr
 
 
 def table_rows(table):
-    return fetchall_query(f"SELECT * FROM {table};")
+    arr = fetchall_query(f"SELECT * FROM {table};")
+    i = 0
+    for str in arr:
+        str_list = list(str)
+        str_list.append("edit")
+        arr[i] = str_list
+        i += 1
+    return arr
 
 def all_tables():
     arr = fetchall_query("SELECT table_name FROM information_schema.tables "
